ae00c63cb112f4a64f35357e6cbc209e5130c5eb
Keith T. Garner <kgarner@kgarner.com>
2010-07-31 15:10:00 -0500

    Move from digitalWrite() to PortManipulation.

--- sketchbook/TrafficLight/LightModel.cpp
+++ sketchbook/TrafficLight/LightModel.cpp
@@ -1,16 +1,9 @@
 #include "WProgram.h"
 #include "LightModel.h"
 
-const int LightModel::RED_LIGHT = 10;
-const int LightModel::AMBER_LIGHT = 11;
-const int LightModel::GREEN_LIGHT = 12;
-
 const byte LightModel::RED_ON = 0x1;
 const byte LightModel::AMBER_ON = 0x2;
 const byte LightModel::GREEN_ON = 0x4;
-
-const bool LightModel::ON = true;
-const bool LightModel::OFF = false;
 
 LightModel::LightModel()
   : state(0)
@@ -20,21 +13,17 @@ LightModel::LightModel()
 // typical AMBER light is 5 seconds
 void LightModel::setup()
 {
-    pinMode(RED_LIGHT, OUTPUT);
-    pinMode(AMBER_LIGHT, OUTPUT);
-    pinMode(GREEN_LIGHT, OUTPUT);
-
-    // Ensure that all lights are off at the start
-    digitalWrite(RED_LIGHT, LOW);
-    digitalWrite(AMBER_LIGHT, LOW);
-    digitalWrite(GREEN_LIGHT, LOW);
+    // Set digital pins 3, 4, and 5 to be output.
+    DDRD = B00011100;
+    // Set all values low
+    PORTD = 0;
 }
 
+// The state is only expecting a value between 0 and 7.  We'll
+// bitshift it over twice to match our input.
 void LightModel::setState(byte newState)
 {
-    state = newState;
+    state = newState << 2;
     
-    digitalWrite(RED_LIGHT, state & RED_ON ? HIGH: LOW);
-    digitalWrite(AMBER_LIGHT, state & AMBER_ON ? HIGH : LOW);
-    digitalWrite(GREEN_LIGHT, state & GREEN_ON ? HIGH : LOW);
+    PORTD = state;
 }        
--- sketchbook/TrafficLight/LightModel.h
+++ sketchbook/TrafficLight/LightModel.h
@@ -11,18 +11,11 @@ class LightModel
 
     void setState(byte newState);
         
-    static const bool ON;
-    static const bool OFF;
-
     static const byte RED_ON;
     static const byte AMBER_ON;
     static const byte GREEN_ON;
 
   private:
-    static const int RED_LIGHT;
-    static const int AMBER_LIGHT;
-    static const int GREEN_LIGHT;
-
     byte state;
 };
 
